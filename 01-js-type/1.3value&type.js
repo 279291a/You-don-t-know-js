4c5518aa77ab3569b73faf86efd563c564242367
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-26 17:17:37 +0800

    使用typeof安全防范机制

--- 01-js-type/1.3value&type.js
+++ 01-js-type/1.3value&type.js
@@ -5,3 +5,16 @@ b;  //ReferenceError: b is not defined
 
 typeof a;   //undefined
 typeof b;   //undefined
+
+//typeof undeclared
+
+//这样会抛出错误
+if(DEBUG){
+  console.log('Debugging is starting');
+}
+
+//这样是安全的
+
+if(typeof DEBUG === 'undefined'){
+  console.log('Debugging is starting');
+}
