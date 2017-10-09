8c5d859e6656c2a54c27705849212921d61ec4f7
王晓丽 <xiaoli.wang@echoesnet.com>
2017-10-09 13:48:34 +0800

    内部属性[[Class]]

--- 03-build-in-function/String.js
+++ 03-build-in-function/String.js
@@ -3,3 +3,8 @@ var a = new String('abc');    //undefined
 typeof a    //"object"
 a instanceof String    //true
 console.log(a);    //VM2036:1 String {0: "a", 1: "b", 2: "c", length: 3, [[PrimitiveValue]]: "abc"}
+
+//内部属性[[Class]],无法直接查看，一般通过Object.prototype.toString()来查看
+
+Object.prototype.toString.call([1,2,3]); //[object Array]
+Object.prototype.toString.call(/js/i);  //[object RegExp]
