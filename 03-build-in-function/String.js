7d944b347304b8ca17767b09a01d956360d20043
王晓丽 <xiaoli.wang@echoesnet.com>
2017-10-09 13:44:35 +0800

    通过原生函数创建出来的是封装了基本类型值的封装对象。

---
+++ 03-build-in-function/String.js
@@ -0,0 +1,5 @@
+/*eslint-disable */
+var a = new String('abc');    //undefined
+typeof a    //"object"
+a instanceof String    //true
+console.log(a);    //VM2036:1 String {0: "a", 1: "b", 2: "c", length: 3, [[PrimitiveValue]]: "abc"}
