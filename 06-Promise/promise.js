bce783a642387e29c768f7c31bc21b3c08cff1f4
王晓丽 <xiaoli.wang@echoesnet.com>
2017-10-27 15:35:07 +0800

    Promise 信任问题

--- 06-Promise/promise.js
+++ 06-Promise/promise.js
@@ -14,26 +14,75 @@ p.then(function fulfilled() {
 
 //promise.then 中发生异常
 
-var o = new Promise(function(resolve,reject){
+var o = new Promise(function (resolve, reject) {
   resolve(42);
 });
 
-var x= o.then(
-  function fulfilled(msg){
+var x = o.then(
+  function fulfilled(msg) {
     foo.bar();
     console.log(msg);//永远不会到达这里
   },
-  function rejected(err){
+  function rejected(err) {
     console.log(err);//永远不会到达这里
   }
 )
 
 x.then(
-  function fulfilled(msg){
+  function fulfilled(msg) {
     foo.bar();
     console.log(msg);
   },
-  function rejected(err){
+  function rejected(err) {
     console.log(err);//永远不会到达这里
   }
 );
+
+//如果向Promise.resolve() 传递一个非Promise、非thenable 的立即值，就会得到一个用这个值填充的Promise。
+//下面的p1,p2行为完全一样
+var p1 = new Promise(function (resolve, reject) {
+  resolve(42);
+});
+var p2 = Promise.resolve(42);
+
+//向Promise.resolve()中传递一个真正的Promise，就会返回同一个Promise
+var p3 = Promise.resolve(42);
+var p4 = Promise.resolve(p3);
+
+p3 === p4;//true
+
+//传递非Promise 的thenable 值，将会试图展开这个值，而且展开过程会持续到提取出一个具体的非类Promise的最终值
+
+var p = {
+  then:function(cb){
+    cb(42);
+  }
+}
+
+p
+  .then(
+    function fulfilled(val){
+      console.log(val);  //42
+    },
+    function rejected(err){
+      console.log(err);  //永远不会运行
+    }
+  )
+
+//另一种
+var p = {
+  then:function(cb,errcb){
+    cb(42);
+    errcb('wrong');
+  }
+};
+
+p
+.then(
+  function fulfilled(val){
+    console.log(val);  //42
+  },
+  function rejected(err){
+    console.log(err);  //wrong
+  }
+)
