c74bcac86f07210aab1b286be42d94bc105a2a2e
王晓丽 <xiaoli.wang@echoesnet.com>
2017-10-27 15:12:58 +0800

    是否吞掉异常

--- 06-Promise/promise.js
+++ 06-Promise/promise.js
@@ -1,13 +1,39 @@
 //吞掉错误或异常
 var p = new Promise(function (resolve, reject) {
   foo.bar();
-  resolve(42);
+  resolve(42);//永远不会到达这里
 });
 
 p.then(function fulfilled() {
-  console.log('fulfill');
+  console.log('fulfill');//永远不会到达这里
 },
   function rejected(err) {
     console.log(err);
   }
 )
+
+//promise.then 中发生异常
+
+var o = new Promise(function(resolve,reject){
+  resolve(42);
+});
+
+var x= o.then(
+  function fulfilled(msg){
+    foo.bar();
+    console.log(msg);//永远不会到达这里
+  },
+  function rejected(err){
+    console.log(err);//永远不会到达这里
+  }
+)
+
+x.then(
+  function fulfilled(msg){
+    foo.bar();
+    console.log(msg);
+  },
+  function rejected(err){
+    console.log(err);//永远不会到达这里
+  }
+);
