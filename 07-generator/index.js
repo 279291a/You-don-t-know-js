89010b75bef33b7341e9faa1e1b30461ecedd898
王晓丽 <xiaoli.wang@echoesnet.com>
2017-10-31 14:13:52 +0800

    实现标准的迭代器接口

--- 07-generator/index.js
+++ 07-generator/index.js
@@ -15,11 +15,11 @@ function* bar() {
   b = a * (yield 2);
 }
 
-function step(gen){
+function step(gen) {
   var it = gen();
   var last;
 
-  return function(){
+  return function () {
     last = it.next(last).value;
   }
 }
@@ -36,3 +36,33 @@ s1();
 
 s1();
 s2();
+
+// 实现标准的迭代器接口
+var something = (function () {
+  var nextVal;
+
+  return {
+    //for..of循环需要
+    [Symbol.iterator]: function () { return this; },
+
+    //标准迭代器接口方法
+    next: function () {
+      if (nextVal === undefined) {
+        nextVal = 1;
+      } else {
+        nextVal = (3 * nextVal) + 6;
+      }
+
+      return { done: false, value: nextVal }
+    }
+  }
+})();
+
+for(var v of something){
+  console.log(v);
+
+  //不要死循环
+  if(v>500){
+    break;
+  }
+}
